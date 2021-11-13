70bd27121f0c86bae2adfb8958cc01f99aebaa2c
olivermarch <march89@gmail.com>
2021-11-13 19:22:42 +0000

    Primer comit de la Actividad 4 para subir el proyecto recién creado

---
+++ Actividad4/app/src/main/java/com/example/actividad4/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.actividad4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
