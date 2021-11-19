a5280efe05b48cab2723f5022f28629ad8ad1358
olivermarch <march89@gmail.com>
2021-11-19 15:36:40 +0000

    Se completa la actividad creando la funcion de sumar que extrae el valor en texto de los editText y los parsea a integer, y finalmente tras realizar la suma lo imprime en el TextView

--- Actividad7/app/src/main/java/com/example/actividad7/MainActivity.java
+++ Actividad7/app/src/main/java/com/example/actividad7/MainActivity.java
@@ -1,14 +1,46 @@
+
 package com.example.actividad7;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editText1;
+    EditText editText2;
+    TextView tvResultado;
+    int num1 = 0;
+    int num2 = 0;
+    int aux = 0 ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText1 = findViewById(R.id.txtFirst);
+        editText2 = findViewById(R.id.txtSecond);
+
+        tvResultado = findViewById(R.id.txtResult);
+
+
     }
+
+    public void sumar(View view) {
+
+        num1 = Integer.parseInt(editText1.getText().toString());
+        num2 = Integer.parseInt(editText2.getText().toString());
+
+        System.out.println("Este es es el primero " +num1);
+        System.out.println("Este es es el segundo " +num2);
+
+        aux = num1 + num2;
+        tvResultado.setText("" + aux );
+        System.out.println(aux);
+    }
+
 }
