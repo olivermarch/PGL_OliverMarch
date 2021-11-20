afe48c48bef8aa9fb5b8725907d0a4c13e3eaad4
olivermarch <march89@gmail.com>
2021-11-20 18:52:19 +0000

    Termino de implementar la lógica para los botones de operaciones, realizao un método que recibe un simbolo de operación según que teclase presione. Finalmente con el botón de igual realizo los calculos dependiendo de que se ha pulsado. Primeramente intenté implementarlo con un switch, pero no conseguí que funcionase ni entiendo porque no funcionaba, lo dejo comentado para una revisión futura, ya que me parecia una solución más optima

--- Actividad9/app/src/main/java/com/example/actividad9/MainActivity.java
+++ Actividad9/app/src/main/java/com/example/actividad9/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.actividad9;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -11,6 +10,10 @@ public class MainActivity extends AppCompatActivity {
 
     TextView tvResultado;
 
+    float dato1 = 0.0f;
+    float dato2 = 0.0f;
+    float resultado = 0.0f;
+    String operacion = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,31 +23,28 @@ public class MainActivity extends AppCompatActivity {
 
         tvResultado = findViewById(R.id.tWResultado);
         //Botonees de numeros de la calculadora
-        findViewById(R.id.btn0).setOnClickListener(v -> setResultado(0));
-        findViewById(R.id.btn1).setOnClickListener(v -> setResultado(1));
-        findViewById(R.id.btn2).setOnClickListener(v -> setResultado(2));
-        findViewById(R.id.btn3).setOnClickListener(v -> setResultado(3));
-        findViewById(R.id.btn4).setOnClickListener(v -> setResultado(4));
-        findViewById(R.id.btn5).setOnClickListener(v -> setResultado(5));
-        findViewById(R.id.btn6).setOnClickListener(v -> setResultado(6));
-        findViewById(R.id.btn7).setOnClickListener(v -> setResultado(7));
-        findViewById(R.id.btn8).setOnClickListener(v -> setResultado(8));
-        findViewById(R.id.btn9).setOnClickListener(v -> setResultado(9));
+        findViewById(R.id.btn0).setOnClickListener(v -> setNumeroEnPantalla(0));
+        findViewById(R.id.btn1).setOnClickListener(v -> setNumeroEnPantalla(1));
+        findViewById(R.id.btn2).setOnClickListener(v -> setNumeroEnPantalla(2));
+        findViewById(R.id.btn3).setOnClickListener(v -> setNumeroEnPantalla(3));
+        findViewById(R.id.btn4).setOnClickListener(v -> setNumeroEnPantalla(4));
+        findViewById(R.id.btn5).setOnClickListener(v -> setNumeroEnPantalla(5));
+        findViewById(R.id.btn6).setOnClickListener(v -> setNumeroEnPantalla(6));
+        findViewById(R.id.btn7).setOnClickListener(v -> setNumeroEnPantalla(7));
+        findViewById(R.id.btn8).setOnClickListener(v -> setNumeroEnPantalla(8));
+        findViewById(R.id.btn9).setOnClickListener(v -> setNumeroEnPantalla(9));
 
         //Botones de calculos
 
         findViewById(R.id.btnBorrado).setOnClickListener(v -> borrar());
-        findViewById(R.id.btnMultiplicacion).setOnClickListener(v -> );
-        findViewById(R.id.btnDivision).setOnClickListener(v -> );
-        findViewById(R.id.btnSuma).setOnClickListener(v -> );
-        findViewById(R.id.btnIgual).setOnClickListener(v -> );
-        findViewById(R.id.btnResta).setOnClickListener(v -> );
-
-
-
+        findViewById(R.id.btnMultiplicacion).setOnClickListener(v -> operar("*"));
+        findViewById(R.id.btnDivision).setOnClickListener(v -> operar("/"));
+        findViewById(R.id.btnSuma).setOnClickListener(v -> operar("+"));
+        findViewById(R.id.btnResta).setOnClickListener(v -> operar("-"));
+        findViewById(R.id.btnIgual).setOnClickListener(v -> resolver());
     }
 
-    public void setResultado(Integer numero){
+    public void setNumeroEnPantalla(Integer numero){
         int pantalla = Integer.parseInt(tvResultado.getText().toString());
         if (pantalla == 0){
             tvResultado.setText(""+numero);
@@ -54,14 +54,48 @@ public class MainActivity extends AppCompatActivity {
         }
     }
     public void borrar(){
-        tvResultado.setText("");
+        tvResultado.setText("0");
     }
-    public void multiplicar(){
-
+    public void operar(String simbolo){
+        dato1 = Float.parseFloat(tvResultado.getText().toString());
+        borrar();
+        operacion = simbolo;
     }
-    public void sumar(){
 
+    public void resolver(){
+        dato2 = Float.parseFloat(tvResultado.getText().toString());
+
+        if (operacion == "+") {
+            resultado = dato1 + dato2;
+            tvResultado.setText(""+ (resultado));
+        }else if(operacion == "-"){
+            resultado = dato1 - dato2;
+            tvResultado.setText(""+ (resultado));
+        }else if(operacion == "/") {
+            resultado = dato1 / dato2;
+            tvResultado.setText("" + (resultado));
+        }else if(operacion == "*") {
+            resultado = dato1 * dato2;
+            tvResultado.setText("" + (resultado));
+        }
+
+//        switch (operacion){
+//            case division:
+//                resultado = dato1 / dato2;
+//                tvResultado.setText(""+ (resultado));
+//
+//            case multipicacion:
+//                resultado = dato1 * dato2;
+//                tvResultado.setText(""+ (resultado));
+//
+//            case suma:
+//                resultado = dato1 + dato2;
+//                tvResultado.setText(""+ (resultado));
+//
+//            case resta:
+//                resultado = dato1 - dato2;
+//                tvResultado.setText(""+ (resultado));
+
+//        }
     }
-
-
 }
