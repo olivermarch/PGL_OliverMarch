b478b38b31d7115f4fb571027ed674cc4ba5868e
olivermarch <march89@gmail.com>
2021-11-21 22:33:29 +0000

    Ha este punto he creado los intent para poder pasar de una activity a otra, asi como la lógica para extraer la información que hay que pasar con los putExtra. Me queda, recibirla en el otro lado y poder imprimirla bien

--- Actividad12/app/src/main/java/com/example/actividad12/IMC_Activity.java
+++ Actividad12/app/src/main/java/com/example/actividad12/IMC_Activity.java
@@ -2,13 +2,30 @@ package com.example.actividad12;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class IMC_Activity extends AppCompatActivity {
+
+
+    Bundle datos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_imc);
+
+        pulsarBoton();
     }
-}
+
+    public void pulsarBoton(){
+
+        findViewById(R.id.btnRegresar).setOnClickListener(v ->{
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+        });
+    }
+
+    }
+
--- Actividad12/app/src/main/java/com/example/actividad12/MainActivity.java
+++ Actividad12/app/src/main/java/com/example/actividad12/MainActivity.java
@@ -2,13 +2,63 @@ package com.example.actividad12;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.RadioButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText etAltura;
+    EditText etEdad;
+    EditText etPeso;
+    RadioGroup rbSexo;
+    RadioButton rbsex;
+
+    Double altura;
+    Double edad;
+    Double peso;
+    Double imc;
+    String sexo = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etAltura = findViewById(R.id.txtAltura);
+        etEdad = findViewById(R.id.txtEdad);
+        etPeso = findViewById(R.id.txtPeso);
+        rbSexo = findViewById(R.id.rbSexoTotal);
+        rbsex =findViewById(R.id.rbHombre);
+
+        pulsarBoton();
+
+    }
+
+    public void pulsarBoton(){
+
+
+        findViewById(R.id.btnCalcular).setOnClickListener(v ->{
+            obtenerDatos();
+            Intent intent = new Intent(getApplicationContext(), IMC_Activity.class);
+            intent.putExtra("altura", altura);
+            intent.putExtra("edad", edad);
+            intent.putExtra("peso", peso);
+            intent.putExtra("sexo", sexo);
+            startActivity(intent);
+        });
+
+    }
+
+    public  void obtenerDatos () {
+        altura = Double.parseDouble(etAltura.getText().toString());
+        edad = Double.parseDouble(etEdad.getText().toString());
+        peso = Double.parseDouble(etPeso.getText().toString());
+        sexo = rbsex.getText().toString();
+        imc = peso / Math.pow(2, altura);
+
+
     }
 }
