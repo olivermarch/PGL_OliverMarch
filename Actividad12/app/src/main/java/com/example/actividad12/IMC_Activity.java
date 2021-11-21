70262e6c335e4162c959177a994951bb2b8bbe23
olivermarch <march89@gmail.com>
2021-11-21 18:33:42 +0000

    Se realiza la interfaz de ambas pantallas de activity, se preparan los id para su uso, pero no se ha hecho aún a este punto, nada de la lógica de programación

---
+++ Actividad12/app/src/main/java/com/example/actividad12/IMC_Activity.java
@@ -0,0 +1,14 @@
+package com.example.actividad12;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class IMC_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_imc);
+    }
+}
