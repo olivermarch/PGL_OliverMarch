d22392a5d7ed27b841ffd534cc20221f12230710
olivermarch <march89@gmail.com>
2021-11-22 14:53:27 +0000

    Implemento la funcion para que los botones cambien el texto al deseado y que tras el segundo click, despues de elegir el numero para apostar, se vuelva a la configuracion inicial, tambien incluyo en el webview el nombre y el valor apostado

--- JuegoApostar/app/src/main/java/com/example/juegoapostar/pantalla_juego.java
+++ JuegoApostar/app/src/main/java/com/example/juegoapostar/pantalla_juego.java
@@ -3,7 +3,10 @@ package com.example.juegoapostar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
 import android.widget.Button;
+import java.util.ArrayList;
+import java.util.List;
 
 public class pantalla_juego extends AppCompatActivity {
     Bundle datos;
@@ -21,14 +24,75 @@ public class pantalla_juego extends AppCompatActivity {
             R.id.btn9,};
 
     private  Button [] boton = new Button[idBotones.length];
+    int apuesta;
+
+    String nombreJugador;
+
+
+   final String[] valoresBotones = {"0-9", "10-19", "20-29", "30-39", "40-49", "50-59", "60-69", "70-79", "80-89", "90-99"};
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla_juego);
 
         datos = getIntent().getExtras();
-        String nombreJugador = datos.getString("nombre");
+        nombreJugador = datos.getString("nombre");
 
+        WebView webView = (WebView) findViewById(R.id.webView);
+        webView.loadDataWithBaseURL(null,agregarHtml(""),"text/html", "utf-8", null);
+        setAllButtons();
+
+    }
+
+    public void setBotones(int num){
+
+        if (num == 0) {
+            for (int i = 0; i < 10; i++) {
+
+                boton[i] = (Button) findViewById(idBotones[i]);
+                boton[i].setText(""+i);
+                int j = i;
+                findViewById(idBotones[i]).setOnClickListener(v->{
+                    apuesta = Integer.parseInt(boton[j].getText().toString());
+                    String apuestaDato = boton[j].getText().toString();
+                    WebView webView = (WebView) findViewById(R.id.webView);
+                    webView.loadDataWithBaseURL(null,agregarHtml(apuestaDato),"text/html", "utf-8", null);
+
+                    for (int k = 0; k < 10; k++) {
+                        boton[k] = (Button) findViewById(idBotones[k]);
+                        boton[k].setText(""+valoresBotones[k]);
+                    }
+                    setAllButtons();
+                });
+
+            }
+        }else{
+            for (int i = 0; i < 10; i++) {
+
+                boton[i] = (Button) findViewById(idBotones[i]);
+                boton[i].setText(""+num+i);
+                int j = i;
+                findViewById(idBotones[i]).setOnClickListener(v->{
+                    apuesta = Integer.parseInt(boton[j].getText().toString());
+                    String apuestaDato = boton[j].getText().toString();
+                    WebView webView = (WebView) findViewById(R.id.webView);
+                    webView.loadDataWithBaseURL(null,agregarHtml(apuestaDato),"text/html", "utf-8", null);
+
+                    for (int k = 0; k < 10; k++) {
+                        boton[k] = (Button) findViewById(idBotones[k]);
+                        boton[k].setText(""+valoresBotones[k]);
+                    }
+                    setAllButtons();
+                });
+
+
+            }
+        }
+    }
+
+    public void setAllButtons (){
         findViewById(R.id.btn0).setOnClickListener(v->{setBotones(0);});
         findViewById(R.id.btn1).setOnClickListener(v->{setBotones(1);});
         findViewById(R.id.btn2).setOnClickListener(v->{setBotones(2);});
@@ -39,22 +103,41 @@ public class pantalla_juego extends AppCompatActivity {
         findViewById(R.id.btn7).setOnClickListener(v->{setBotones(7);});
         findViewById(R.id.btn8).setOnClickListener(v->{setBotones(8);});
         findViewById(R.id.btn9).setOnClickListener(v->{setBotones(9);});
-
     }
 
-    public void setBotones(int num){
+    String agregarHtml(String texto){
 
-        if (num == 0) {
-            for (int i = 0; i < 10; i++) {
-                boton[i] = (Button) findViewById(idBotones[i]);
-                boton[i].setText(""+i);
-            }
-        }else{
-            for (int i = 0; i < 10; i++) {
-                boton[i] = (Button) findViewById(idBotones[i]);
-                boton[i].setText(""+num+i);
+        String resultado =
+                "<!DOCTYPE html >"+
+                        "<html>"+
+                        "<head>"+
 
-            }
-        }
+                        "<meta charset='utf-8' />"+
+                        "<meta name='author' content='Oliver March'/>"+
+                        "<meta name='viewport' content='width=device-width, initial-scale=1.0'/>"+
+                        "<title>Credenciales de la persona</title>"+
+
+                        "<style type='text/css' media='screen'>"+
+                        "*{ text-align: center;}"+
+                        "body{background: black;}"+
+                        "h1 {color: #DEDCDC; font-style: italic }"+
+                        "p {color: white;}"+
+                        "</style>"+
+
+                        "</head>"+
+                        "<body>"+
+                        "<h1>"+
+                        nombreJugador +
+                        "</h1>"+
+                        "<p>+" +
+                        texto +
+                        "</p>"+
+                        "</body>"+
+                        "</html>";
+
+
+        return resultado;
+
+
     }
 }
